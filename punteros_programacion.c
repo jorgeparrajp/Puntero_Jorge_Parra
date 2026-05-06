2c42057f00ad2bf00fa97cfbd6fecc6614fe41b6
Jorge Parra <jorgeenriqueparrachora@gmail.com>
2026-05-06 14:54:10 -0500

    Agregamos un flotante

--- punteros_programacion.c
+++ punteros_programacion.c
@@ -2,13 +2,24 @@
 
 int main (){
  int num, a ;
- int *x ; // puntero 
+ int *x ; //puntero 
 num =  150 ; 
 
 
- x = &num ; // almacena la direcion de memoria de num 
+ x = &num ; //almacena la direcion de memoria de num 
 
- printf ("%d %p", num, x );
+ printf ("%d %p  ", num, x );
+
+
+
+ float pi = 3.1416, decimal;
+
+ float *ptrF; //definicion de puntero
+
+ ptrF = &pi ;
+
+ printf (" %f %p", pi , ptrF);
+
 
 
 
